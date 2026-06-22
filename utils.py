e0fc7c9fd57c06a5d9c9ecc70c8bada2b73f89ad
Sakhawat <saimon.sust@gmail.com>
2026-06-22 18:23:11 -0500

    Added utility classes for different threshold selection strategies and caching results

--- utils.py
+++ utils.py
@@ -6,21 +6,57 @@ Created on Wed Dec  4 11:16:56 2024
 @author: Sakhawat
 """
 
+from abc import ABC, abstractmethod
 import numpy as np
 import pandas as pd
-from scipy.io import loadmat
-from sklearn.metrics import (roc_auc_score,
-                             roc_curve,
-                             f1_score,
-                             balanced_accuracy_score,
-                             cohen_kappa_score)
+import scipy
+from scipy.spatial.distance import squareform
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+from sklearn.metrics import (
+    roc_auc_score,
+    f1_score,
+    balanced_accuracy_score,
+    cohen_kappa_score)
+from pathlib import Path
 
 # Utility function
 def format_time(seconds):
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
-    return f"{int(hours):,}h {int(minutes)}m {int(seconds)}s"
+    hours_str = f"{int(hours):,}"
+    return f"{hours_str:>5s}h {int(minutes):02d}m {int(seconds):02d}s"
 
+def cache_to_file(filepath, val, fmt = "%.6f", **kwargs):
+    filepath = Path(filepath)
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    if isinstance(val, pd.DataFrame) or isinstance(val, pd.Series):
+        val.to_csv(filepath, header = None, sep = '\t', float_format = fmt, **kwargs)
+    else:
+        np.savetxt(filepath, np.array(val), fmt = fmt, **kwargs)
+    
+def save_feature_importances(clf, filename, as_sparse = True):
+    feature_importances = None
+    if isinstance(clf, LogisticRegression):
+        feature_importances = clf.coef_.ravel()
+    elif isinstance(clf, (RandomForestClassifier, xgb.XGBClassifier)):
+        feature_importances = clf.feature_importances_
+    else:
+        try:
+            feature_importances = clf.feature_importances_
+        except:
+            pass
+    if feature_importances is None:
+        return
+    Path(filename).parent.mkdir(parents=True, exist_ok=True)
+    if as_sparse:
+        scipy.sparse.save_npz(
+            filename,
+            scipy.sparse.csr_matrix(feature_importances))
+    else:
+        np.savetxt(filename + '.csv', feature_importances, fmt = '%.8f')
+        
 # Utility function
 def scores_to_metrics(scores, y_true, p_threshold = 0.5):
     '''
@@ -103,4 +139,45 @@ class BaselineTransformation:
     
     def transform(self, X, **kwargs):
         return X
+
+
+class ThresholdSelector(ABC):
+    def __init__(self):
+        self.threshold_labels = []
     
+    def get_threshold_labels(self):
+        return self.threshold_labels
+    
+    @abstractmethod
+    def get_thresholds(self, X):
+        pass
+    
+class FixedThresholdSelector(ThresholdSelector):
+    '''Fixed thresholds for training local models'''
+    def __init__(self, H = None):
+        if H is None:
+            H = np.arange(0.15, 0.25+0.025, 0.025).round(3)
+        self.threshold_labels = H.astype(str)
+        self.H = H
+        
+    def get_thresholds(self, as_series = False):
+        if as_series:
+            return pd.Series(self.H, index = self.get_threshold_labels())
+        return self.H
+    
+class PercentileThresholdSelector(ThresholdSelector):
+    '''Percentile-based thresholds for training local models'''
+    def __init__(self, low_percentile = 65, high_percentile = 90, n_thresholds = 6):
+        self.low_percentile = low_percentile
+        self.high_percentile = high_percentile
+        self.n_thresholds = n_thresholds
+        self.percentiles = np.linspace(self.low_percentile, self.high_percentile, n_thresholds)
+        self.threshold_labels = self.percentiles.round(3).astype(str)
+        
+    def get_thresholds(self, X, corr = None, as_series = False):
+        if corr is None:
+            corr = squareform(np.corrcoef(X), checks = False)
+        H = np.array([np.percentile(np.abs(corr), p) for p in self.percentiles])
+        if as_series: 
+            return pd.Series(H, index = self.get_threshold_labels())
+        return H
